2e71100a60f25e2f9a5f7f0cbbb04643d06b9fd1
Siliconpot <piipurkki@gmail.com>
2016-02-09 12:12:13 +0200

    lisätty main menu, musiikkia, yms

---
+++ unityProject04/Assets/Scripts/startGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startGame : MonoBehaviour {
+
+	void OnMouseOver () {
+
+		if (Input.GetMouseButton (0))
+			Application.LoadLevel("GameScene");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
